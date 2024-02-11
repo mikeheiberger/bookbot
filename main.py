5520e6d94c4c083faef83324edfb3f8605f6dae0
mikeheiberger <michael.heiberger@gmail.com>
2024-02-10 19:09:16 -0700

    Adds report printing with word count and character counts, sorted from most to least

--- main.py
+++ main.py
@@ -3,7 +3,15 @@ def main():
     book_path = "books/frankenstein.txt"
     text = read_book(book_path)
     word_count = count_words(text)
-    print(word_count)
+    letters = letter_count(text)
+    letters.sort(reverse=True, key=sort_on)
+
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{word_count} words found in the document\n")
+    for sorted_letter in letters:
+        print(f"The {sorted_letter["letter"]} was found {sorted_letter["count"]} times")
+    print("--- End report ---")
+    
 
 def read_book(path):
     with open(path) as f:
@@ -12,5 +20,25 @@ def read_book(path):
 def count_words(text):
     words = text.split()
     return len(words)
+
+def letter_count(text):
+    letters_with_count = {}
+    for character in text:
+        lower = character.lower()
+        if lower.isalpha():
+            if lower not in letters_with_count:
+                letters_with_count[lower] = 1
+            else:
+                letters_with_count[lower] += 1
+
+    letters_with_count_list = []
+    for letter in letters_with_count:
+        letter_with_count = { "letter": letter, "count": letters_with_count[letter]}
+        letters_with_count_list.append(letter_with_count)
+
+    return letters_with_count_list
+
+def sort_on(dict):
+    return dict["count"]
 
 main()
